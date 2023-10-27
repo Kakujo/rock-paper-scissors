7df9a1ca5ac6179fed156b769a3f979b7e80064f
Kakujo <kakujomics.media@gmail.com>
2023-10-27 15:19:40 -0700

    add basic rock paper scissors game

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Making a rock-paper-scissors game for The Odin Project. Let's goooooo
+Input
+The user is PROMPTed to choose (enter) either "paper", "scissors" or "rock"
+
+
+Algorithm
+The computer randomly chooses "paper", "scissors" or "rock".
+The user's choice is tested against the computer's random selection.
+If the user's selection beats the computer's selection via the rules of rock-paper-scissors, the user wins.
+ Otherwise, the user loses.
+
+
+Output
+The user is alerted to the results via console.log() (client requirement)
+The results are stored in a variable to track number of wins.
+Another variable keeps track of how many times the game has been played, for fun.
+The final victor is based on who scores 5 first.
--- script.js
+++ script.js
@@ -1,1 +1,107 @@
 console.log("Hello, World!");
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    console.log("randomNumber is: " + randomNumber);
+    if (randomNumber === 3) {
+        return "rock";
+    } else if (randomNumber === 2) {
+        return "paper";
+    }
+    return "scissors";
+}
+
+console.log("Testing... Computer's choice is: " + getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+
+        if (computerSelection === "rock") return 1;
+        if (computerSelection === "paper") return 2;
+        if (computerSelection === "scissors") return 3;
+
+    } else if (playerSelection === "paper") {
+
+        if (computerSelection === "rock") return 4;
+        if (computerSelection === "paper") return 5;
+        if (computerSelection === "scissors") return 6;
+
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") return 7;
+        if (computerSelection === "paper") return 8;
+        if (computerSelection === "scissors") return 9;
+    }
+    return false;
+}
+
+function game() {
+    alert("First to 5 wins!");
+    let playerScore = 0;
+    let computerScore = 0;
+    let playCount = 0;
+    let victor = "";
+    while (playerScore < 5 && computerScore < 5) {
+        const playerSelection = prompt("Okay, round #" + (playCount + 1) + "! Do you choose Paper, Scissors or Rock?").toLowerCase();
+        console.log("Player entered: "+ playerSelection);
+        const computerSelection = getComputerChoice();
+        console.log("Computer entered: " + computerSelection);
+        const roundResult = playRound(playerSelection, computerSelection);
+
+        if (roundResult) {
+            switch (roundResult) {
+                case 1:
+                    console.log("Tie! Rock and Rock!");
+                    break;
+
+                case 2:
+                    console.log("You lose! Paper beats Rock!");
+                    computerScore += 1;
+                    break;
+
+                case 3:
+                    console.log("You win! Rock beats Scissors!");
+                    playerScore += 1;
+                    break;
+
+                case 4:
+                    console.log("You win! Paper beats Rock!");
+                    playerScore += 1;
+                    break;
+
+                case 5:
+                    console.log("Tie! Paper and Paper!");
+                    break;
+
+                case 6:
+                    console.log("You lose! Scissors beats Paper!");
+                    computerScore += 1;
+                    break;
+
+                case 7:
+                    console.log("You lose! Rock beats Scissors!");
+                    computerScore += 1;
+                    break;
+
+                case 8:
+                    console.log("You win! Scissors beats Paper!");
+                    playerScore += 1;
+                    break;
+
+                case 9:
+                    console.log("Tie! Scissors and Scissors!");
+                    break;
+            }
+            playCount += 1;
+        } else {
+            alert("That's not one of the selections! Try again.");
+        }
+    }
+    if (playerScore > computerScore) {
+        victor = "Player";
+    } else {
+        victor = "Computer";
+    }
+    alert("Alright, " + playCount + " rounds later: " + victor + " wins!");
+    
+}
+game();
